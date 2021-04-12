8392c40596ef2a58b499ef599e0917725d25fdf7
Rabiul isalm <robecse99@gmail.com>
2021-04-12 21:12:45 +0600

    Update Api

--- app/User.php
+++ app/User.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App;
-
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -18,7 +17,6 @@ class User extends Authenticatable  implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        
         'name', 'email', 'password',
     ];
 
@@ -30,7 +28,6 @@ class User extends Authenticatable  implements JWTSubject
     protected $hidden = [
         'password', 'remember_token',
     ];
-
     /**
      * The attributes that should be cast to native types.
      *
--- config/auth.php
+++ config/auth.php
@@ -41,6 +41,7 @@ return [
             'provider' => 'users',
         ],
 
+        
         'api' => [
             'driver' => 'jwt',
             'provider' => 'users',
